d792ecb27e93cd42ceb78f49b9371af94ea19c99
cumbak1823 <cumbak1823@gmail.com>
2022-06-16 17:45:49 +0300

    java updated

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Java_v2");
+        System.out.println("Hello from Java_Kaya Cann");
     }
 }
